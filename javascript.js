0ac5aeba26c6f7c69e57f2448754a84eadd4f167
Martin Ruino <martinruino@gmail.com>
2023-11-03 08:48:23 -0300

    Parte de javascript

---
+++ javascript.js
@@ -0,0 +1,22 @@
+const PRECIO=200;
+function calcular_precio(precio,cantidad,descuento){
+    cantidad=Math.round(Number(cantidad));
+    if (typeof cantidad !== 'number' || isNaN(cantidad)) {
+        return ""
+    }
+    descuento=100-Math.round(Number(descuento)); 
+    return precio*cantidad*descuento/100;
+}
+
+function imprimir(){
+    document.getElementById("mostrar_precio").value="Total a pagar: $ "+calcular_precio(PRECIO,document.getElementById("cantidad").value,document.getElementById("categoria").value);
+}
+function borrar_datos(){
+    document.getElementById("nombre").value=""
+    document.getElementById("apellido").value=""
+    document.getElementById("correo").value=""
+    document.getElementById("cantidad").value=""
+    document.getElementById("mostrar_precio").value="Total a pagar: $ "
+}
+document.getElementById("resumen").addEventListener("click",imprimir);
+document.getElementById("borrar").addEventListener("click",borrar_datos);
